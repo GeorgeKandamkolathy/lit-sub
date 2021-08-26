4ad00440ca62a17e780922577c8f65f28ddec663
george <george.kandamkoalthy@gmail.com>
2021-08-26 20:22:45 +1000

    Made user migration

--- lit_sub_backend/lit_sub_backend/settings.py
+++ lit_sub_backend/lit_sub_backend/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'user.apps.UserConfig',
 ]
 
 MIDDLEWARE = [
@@ -100,6 +101,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'user.User'
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
