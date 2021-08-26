aab55c6517ad5242e69d3d6df7ff88c792d09455
george <george.kandamkoalthy@gmail.com>
2021-08-26 20:25:09 +1000

    Added user serializer and url patterns

---
+++ lit_sub_backend/user/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'user'
+urlpatterns = [
+    path('', views.all_view.as_view(), name='authors'),
+    path('<int:author_id>/', views.author_view.as_view(), name='detail'),
+]
